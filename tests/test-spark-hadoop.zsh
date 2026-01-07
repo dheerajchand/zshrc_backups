cbd795defaa0752615fd7d861d16139eda2c943b
Dheeraj Chand <GITHUBEMAIL>
2026-01-07 14:56:16 -0600

    [2026-01-07 14:56:16] Fix HADOOP_CONF_DIR override

--- tests/test-spark-hadoop.zsh
+++ tests/test-spark-hadoop.zsh
@@ -32,7 +32,18 @@ test_hadoop_home_sdkman_preferred() {
     assert_equal "$HOME/.sdkman/candidates/hadoop/current" "$out" "should prefer SDKMAN Hadoop path"
 }
 
+test_hadoop_conf_dir_overrides_invalid() {
+    local tmp out
+    tmp="$(mktemp -d)"
+    mkdir -p "$tmp/etc/hadoop"
+    touch "$tmp/etc/hadoop/core-site.xml"
+    out="$(HADOOP_HOME="$tmp" HADOOP_CONF_DIR="/etc/hadoop" ZSH_TEST_MODE=1 zsh -fc "source $ROOT_DIR/modules/hadoop.zsh; print -r -- \"\$HADOOP_CONF_DIR\"" | tail -n 1)"
+    assert_equal "$tmp/etc/hadoop" "$out" "should override invalid HADOOP_CONF_DIR"
+    rm -rf "$tmp"
+}
+
 register_test "test_spark_home_sdkman_preferred" "test_spark_home_sdkman_preferred"
 register_test "test_hadoop_home_sdkman_preferred" "test_hadoop_home_sdkman_preferred"
 register_test "test_spark_health_defined" "test_spark_health_defined"
 register_test "test_hadoop_health_defined" "test_hadoop_health_defined"
+register_test "test_hadoop_conf_dir_overrides_invalid" "test_hadoop_conf_dir_overrides_invalid"
